2bec693700b423857bc3352058a2256d94722e2b
Dominik277 <dominikdoric277@gmail.com>
2021-06-12 14:29:56 +0200

    Prvi commit.

---
+++ app/src/main/java/hr/dominik/ribolovnodrustvojaksic/MainActivity.kt
@@ -0,0 +1,11 @@
+package hr.dominik.ribolovnodrustvojaksic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
