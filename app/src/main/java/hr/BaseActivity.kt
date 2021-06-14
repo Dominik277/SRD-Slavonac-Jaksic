7c807eff2a66e2faf9274478129e381576ac6585
Dominik277 <dominikdoric277@gmail.com>
2021-06-14 07:20:06 +0200

    Dodan view binding u BaseActivity.kt i kreirana funkcija za prikazivanje SnackBar-a.

--- app/src/main/java/hr/BaseActivity.kt
+++ app/src/main/java/hr/BaseActivity.kt
@@ -2,11 +2,42 @@ package hr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 import hr.dominik.ribolovnodrustvojaksic.R
+import hr.dominik.ribolovnodrustvojaksic.databinding.ActivityBaseBinding
 
 class BaseActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBaseBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_base)
+        binding = ActivityBaseBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
+
+    fun showErrorSnackBar(message: String, errorMessage: Boolean){
+        val snackBar = Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
+        val snackBarView = snackBar.view
+
+        if (errorMessage){
+            snackBarView.setBackgroundColor(
+                ContextCompat.getColor(
+                    this,
+                    R.color.colorSnackBarError
+                )
+            )
+        }else{
+            snackBarView.setBackgroundColor(
+                ContextCompat.getColor(
+                    this,
+                    R.color.colorSnackBarSuccess
+                )
+            )
+        }
+        snackBar.show()
+    }
+
 }
