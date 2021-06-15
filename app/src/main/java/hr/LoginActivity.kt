93adb36dde41f2984d633c33f727f0b2dd25ca85
Dominik277 <dominikdoric277@gmail.com>
2021-06-15 12:24:13 +0200

    Napravljena funkcija za provjeravanje lozinke i email-a.

--- app/src/main/java/hr/LoginActivity.kt
+++ app/src/main/java/hr/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
@@ -30,12 +31,6 @@ class LoginActivity : BaseActivity(), View.OnClickListener {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         }
-
-        binding.tvRegister.setOnClickListener {
-            val intent = Intent(this,RegisterActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
     }
 
     override fun onClick(view: View?) {
@@ -55,5 +50,20 @@ class LoginActivity : BaseActivity(), View.OnClickListener {
         }
     }
 
-
+    private fun validateLoginDetails(): Boolean{
+        return when {
+            TextUtils.isEmpty(binding.etEmail.text.toString().trim { it <= ' '}) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email),true)
+                false
+            }
+            TextUtils.isEmpty(binding.etPassword.text.toString().trim { it <= ' '}) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password),true)
+                false
+            }
+            else -> {
+                showErrorSnackBar("Your details are valid.", false)
+                true
+            }
+        }
+    }
 }
