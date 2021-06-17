2e246458cd22ff0f11c8a342f6ab4b2b554c3731
Dominik277 <dominikdoric277@gmail.com>
2021-06-17 10:33:41 +0200

    Uspjesno implementirano da nas prilikom klika na forgot password odvede u ForgotPasswordActivity.kt.

--- app/src/main/java/hr/activities/ForgotPasswordActivity.kt
+++ app/src/main/java/hr/activities/ForgotPasswordActivity.kt
@@ -3,10 +3,30 @@ package hr.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import hr.dominik.ribolovnodrustvojaksic.R
+import hr.dominik.ribolovnodrustvojaksic.databinding.ActivityForgotPasswordBinding
 
 class ForgotPasswordActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityForgotPasswordBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forgot_password)
+        binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setupActionBar()
     }
+
+    private fun setupActionBar(){
+        setSupportActionBar(binding.toolbarForgotPasswordActivity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24)
+        }
+        binding.toolbarForgotPasswordActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+
 }
--- app/src/main/java/hr/activities/LoginActivity.kt
+++ app/src/main/java/hr/activities/LoginActivity.kt
@@ -41,7 +41,8 @@ class LoginActivity : BaseActivity(), View.OnClickListener {
         if (view != null){
             when(view.id){
                 R.id.tv_forgot_password -> {
-
+                    val intent = Intent(this, ForgotPasswordActivity::class.java)
+                    startActivity(intent)
                 }
                 R.id.btn_login -> {
                     logInRegisteredUser()
