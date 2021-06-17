e8e83c72ac4b8e373b7f9e51aef0fac98afcc4c1
Dominik277 <dominikdoric277@gmail.com>
2021-06-17 11:23:41 +0200

    Kreirana model klasa koja ce predstavljati sve podatke o korisniku.

---
+++ app/src/main/java/hr/model/User.kt
@@ -0,0 +1,12 @@
+package hr.model
+
+data class User (
+    val id: String = "",
+    val firstName: String = "",
+    val lastName: String = "",
+    val email: String = "",
+    val image: String = "",
+    val mobile: String = "",
+    val gender: String = "",
+    val profileCompleted: Int = 0
+        )
