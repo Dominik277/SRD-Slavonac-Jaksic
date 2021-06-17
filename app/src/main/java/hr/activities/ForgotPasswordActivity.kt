f0931967ecadfd00fac056b0d75474e5b5d10f27
Dominik277 <dominikdoric277@gmail.com>
2021-06-17 10:45:47 +0200

    Implementirana logika za omogucavanjem slanja nove lozinke.

--- app/src/main/java/hr/activities/ForgotPasswordActivity.kt
+++ app/src/main/java/hr/activities/ForgotPasswordActivity.kt
@@ -2,10 +2,12 @@ package hr.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import hr.dominik.ribolovnodrustvojaksic.R
 import hr.dominik.ribolovnodrustvojaksic.databinding.ActivityForgotPasswordBinding
 
-class ForgotPasswordActivity : AppCompatActivity() {
+class ForgotPasswordActivity : BaseActivity() {
 
     private lateinit var binding: ActivityForgotPasswordBinding
 
@@ -27,6 +29,29 @@ class ForgotPasswordActivity : AppCompatActivity() {
             actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24)
         }
         binding.toolbarForgotPasswordActivity.setNavigationOnClickListener { onBackPressed() }
+
+        binding.btnSubmit.setOnClickListener {
+            val email: String = binding.etEmail.text.toString().trim { it <= ' '}
+            if (email.isEmpty()){
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email),true)
+            }else{
+                showProgressDialog()
+                FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+                    .addOnCompleteListener { task ->
+                        hideProgressDialog()
+                        if (task.isSuccessful){
+                            Toast.makeText(
+                                this,
+                                resources.getString(R.string.email_sent_success),
+                                Toast.LENGTH_LONG
+                            ).show()
+                            finish()
+                        }else{
+                            showErrorSnackBar(task.exception!!.message.toString(),true)
+                        }
+                    }
+            }
+        }
     }
 
 }
