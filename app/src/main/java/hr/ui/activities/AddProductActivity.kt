07eaaa92dd5fe713ed4c73a837168fe961ee3643
Dominik277 <dominikdoric277@gmail.com>
2021-06-20 13:03:07 +0200

    Implementiran do kraja AddProductActivity.kt.

--- app/src/main/java/hr/ui/activities/AddProductActivity.kt
+++ app/src/main/java/hr/ui/activities/AddProductActivity.kt
@@ -3,10 +3,30 @@ package hr.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import hr.dominik.ribolovnodrustvojaksic.R
+import hr.dominik.ribolovnodrustvojaksic.databinding.ActivityAddProductBinding
 
 class AddProductActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddProductBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_product)
+        binding = ActivityAddProductBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        setupActionBar()
     }
+
+    private fun setupActionBar(){
+        setSupportActionBar(binding.toolbarAddProductActivity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_black_24)
+        }
+
+        binding.toolbarAddProductActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+
 }
