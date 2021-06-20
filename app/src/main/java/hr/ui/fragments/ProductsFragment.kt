631756ee21c7f2cb4f45b95efaf82c8047550b26
Dominik277 <dominikdoric277@gmail.com>
2021-06-21 00:04:25 +0200

    Implementirano da se prikazuju podatci unutar recycler view-a.

--- app/src/main/java/hr/ui/fragments/ProductsFragment.kt
+++ app/src/main/java/hr/ui/fragments/ProductsFragment.kt
@@ -4,11 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import androidx.recyclerview.widget.LinearLayoutManager
 import hr.dominik.ribolovnodrustvojaksic.R
 import hr.dominik.ribolovnodrustvojaksic.databinding.FragmentProductsBinding
 import hr.firestore.FirestoreClass
 import hr.model.Product
 import hr.ui.activities.AddProductActivity
+import hr.ui.adapters.MyProductsListAdapter
 
 class ProductsFragment : BaseFragment() {
 
@@ -38,8 +40,17 @@ class ProductsFragment : BaseFragment() {
     fun successProductsListFromFireStore(productsList: ArrayList<Product>){
         hideProgressDialog()
 
-        for (i in productsList){
-            Log.i("Product name",i.title)
+        if (productsList.size > 0){
+            binding.rvMyProductItems.visibility = View.VISIBLE
+            binding.tvNoProductsFound.visibility = View.GONE
+
+            binding.rvMyProductItems.layoutManager = LinearLayoutManager(activity)
+            binding.rvMyProductItems.setHasFixedSize(true)
+            val adapterProducts = MyProductsListAdapter(requireActivity(),productsList)
+            binding.rvMyProductItems.adapter = adapterProducts
+        }else{
+            binding.rvMyProductItems.visibility = View.GONE
+            binding.tvNoProductsFound.visibility = View.VISIBLE
         }
     }
 
