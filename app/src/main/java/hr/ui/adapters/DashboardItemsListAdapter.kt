a4bc162ecc48bbe214505956d0ca31812b242a61
Dominik277 <dominikdoric277@gmail.com>
2021-06-21 10:12:09 +0200

    Kreiran adapter za DashboardActivity.

---
+++ app/src/main/java/hr/ui/adapters/DashboardItemsListAdapter.kt
@@ -0,0 +1,39 @@
+package hr.ui.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import hr.dominik.ribolovnodrustvojaksic.databinding.ItemListLayoutBinding
+import hr.model.Product
+import hr.util.GlideLoader
+
+open class DashboardItemsListAdapter(
+    private val context: Context,
+    private var list: ArrayList<Product>
+): RecyclerView.Adapter<DashboardItemsListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemListLayoutBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val model = list[position]
+        GlideLoader(context).loadProductPicture(model.image,holder.binding.ivItemImage)
+        holder.binding.tvItemName.text = model.title
+        holder.binding.tvItemPrice.text = "$${model.price}"
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    class ViewHolder(val binding: ItemListLayoutBinding):
+        RecyclerView.ViewHolder(binding.root)
+}
