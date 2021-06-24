e0eed156a19b01ad075d25a39e12e160ce42e059
Dominik277 <dominikdoric277@gmail.com>
2021-06-24 21:16:48 +0200

    Implementirano da se prikazuju podaci unutar recyclerview-a.

--- app/src/main/java/hr/ui/activities/CartListActivity.kt
+++ app/src/main/java/hr/ui/activities/CartListActivity.kt
@@ -1,10 +1,13 @@
 package hr.ui.activities
 
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import hr.dominik.ribolovnodrustvojaksic.R
 import hr.dominik.ribolovnodrustvojaksic.databinding.ActivityCartListBinding
 import hr.firestore.FirestoreClass
 import hr.model.CartItem
+import hr.ui.adapters.CartItemsListAdapter
 
 class CartListActivity : BaseActivity() {
 
@@ -25,6 +28,23 @@ class CartListActivity : BaseActivity() {
 
     fun successCartItemsList(cartList: ArrayList<CartItem>){
         hideProgressDialog()
+
+        if (cartList.size > 0){
+            binding.rvCartItemsList.visibility = View.VISIBLE
+            binding.llCheckout.visibility = View.VISIBLE
+            binding.tvNoCartItemFound.visibility = View.GONE
+
+            binding.rvCartItemsList.layoutManager = LinearLayoutManager(this)
+            binding.rvCartItemsList.setHasFixedSize(true)
+            val cartListAdapter = CartItemsListAdapter(this,cartList)
+            binding.rvCartItemsList.adapter = cartListAdapter
+            var subTotal: Double = 0.0
+            for (item in cartList){
+                val price = item.price.toDouble()
+                val quantity = item.cart_quantity.toInt()
+                subTotal += (price * quantity)
+            }
+        }
     }
 
     private fun getCartItemsList(){
--- app/src/main/java/hr/ui/adapters/CartItemsListAdapter.kt
+++ app/src/main/java/hr/ui/adapters/CartItemsListAdapter.kt
@@ -1,28 +1,41 @@
 package hr.ui.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import hr.dominik.ribolovnodrustvojaksic.databinding.ItemCartLayoutBinding
 import hr.model.CartItem
+import hr.util.GlideLoader
 
 open class CartItemsListAdapter(
     private val context: Context,
     private var list: ArrayList<CartItem>
-): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+): RecyclerView.Adapter<CartItemsListAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemCartLayoutBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val model = list[position]
+        GlideLoader(context).loadProductPicture(model.image,holder.binding.ivCartItemImage)
+        holder.binding.tvCartItemTitle.text = model.title
+        holder.binding.tvCartItemPrice.text = "$${model.price}"
+        holder.binding.tvCartQuantity.text = model.cart_quantity
     }
 
     override fun getItemCount(): Int {
-
+        return list.size
     }
 
     class ViewHolder(val binding: ItemCartLayoutBinding): RecyclerView.ViewHolder(binding.root)
+
 
 }
