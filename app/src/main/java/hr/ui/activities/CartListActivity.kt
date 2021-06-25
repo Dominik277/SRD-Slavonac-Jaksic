c2ca06b2ac70fd3a49f90a924e8720f5c666a722
Dominik277 <dominikdoric277@gmail.com>
2021-06-25 21:37:18 +0200

    Dodana funkcija koja ce prikazivati uspjeh zadataka.

--- app/src/main/java/hr/ui/activities/CartListActivity.kt
+++ app/src/main/java/hr/ui/activities/CartListActivity.kt
@@ -7,6 +7,7 @@ import hr.dominik.ribolovnodrustvojaksic.R
 import hr.dominik.ribolovnodrustvojaksic.databinding.ActivityCartListBinding
 import hr.firestore.FirestoreClass
 import hr.model.CartItem
+import hr.model.Product
 import hr.ui.adapters.CartItemsListAdapter
 
 class CartListActivity : BaseActivity() {
@@ -26,20 +27,20 @@ class CartListActivity : BaseActivity() {
         getCartItemsList()
     }
 
-    fun successCartItemsList(cartList: ArrayList<CartItem>){
+    fun successCartItemsList(cartList: ArrayList<CartItem>) {
         hideProgressDialog()
 
-        if (cartList.size > 0){
+        if (cartList.size > 0) {
             binding.rvCartItemsList.visibility = View.VISIBLE
             binding.llCheckout.visibility = View.VISIBLE
             binding.tvNoCartItemFound.visibility = View.GONE
 
             binding.rvCartItemsList.layoutManager = LinearLayoutManager(this)
             binding.rvCartItemsList.setHasFixedSize(true)
-            val cartListAdapter = CartItemsListAdapter(this,cartList)
+            val cartListAdapter = CartItemsListAdapter(this, cartList)
             binding.rvCartItemsList.adapter = cartListAdapter
             var subTotal: Double = 0.0
-            for (item in cartList){
+            for (item in cartList) {
                 val price = item.price.toDouble()
                 val quantity = item.cart_quantity.toInt()
                 subTotal += (price * quantity)
@@ -47,33 +48,36 @@ class CartListActivity : BaseActivity() {
             binding.tvSubTotal.text = "$$subTotal"
             binding.tvShippingCharge.text = "$10.0"
 
-            if (subTotal > 0){
+            if (subTotal > 0) {
                 binding.llCheckout.visibility = View.VISIBLE
 
                 val total = subTotal + 10
                 binding.tvTotalAmount.text = "$$total"
-            }else{
+            } else {
                 binding.llCheckout.visibility = View.GONE
             }
-        }
-        else{
+        } else {
             binding.rvCartItemsList.visibility = View.GONE
             binding.llCheckout.visibility = View.GONE
             binding.tvNoCartItemFound.visibility = View.VISIBLE
         }
     }
 
-    private fun getCartItemsList(){
+    fun successProductsListFromFirestore(productList: ArrayList<Product>) {
+
+    }
+
+    private fun getCartItemsList() {
         showProgressDialog()
         FirestoreClass().getCartList(this)
     }
 
-    private fun setupActionBar(){
+    private fun setupActionBar() {
 
         setSupportActionBar(binding.toolbarCartListActivity)
 
         val actionBar = supportActionBar
-        if (actionBar != null){
+        if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true)
             actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24)
         }
