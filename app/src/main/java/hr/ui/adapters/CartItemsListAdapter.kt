01d0244bdd76d4683a43fa277f463663f290af4e
Dominik277 <dominikdoric277@gmail.com>
2021-06-26 17:10:29 +0200

    Implementirano da korisnik ne moze unijeti vise komada nekog produkta od onoliko koliko ima na skladistu.

--- app/src/main/java/hr/ui/adapters/CartItemsListAdapter.kt
+++ app/src/main/java/hr/ui/adapters/CartItemsListAdapter.kt
@@ -10,6 +10,7 @@ import hr.dominik.ribolovnodrustvojaksic.databinding.ItemCartLayoutBinding
 import hr.firestore.FirestoreClass
 import hr.model.CartItem
 import hr.ui.activities.CartListActivity
+import hr.util.Constants
 import hr.util.GlideLoader
 
 open class CartItemsListAdapter(
@@ -68,13 +69,42 @@ open class CartItemsListAdapter(
         }
 
         holder.binding.ibRemoveCartItem.setOnClickListener {
+            if (model.cart_quantity == "1"){
+                FirestoreClass().removeItemFromCart(context, model.id)
+            }else{
+                val cartQuantity: Int = model.cart_quantity.toInt()
+                val itemHashMap = HashMap<String,Any>()
 
+                itemHashMap[Constants.CART_QUANTITY] = (cartQuantity - 1).toString()
+
+                if (context is CartListActivity){
+                    context.showProgressDialog()
+                }
+                FirestoreClass().updateMyCart(context, model.id, itemHashMap)
+            }
         }
 
         holder.binding.ibAddCartItem.setOnClickListener {
-            
-        }
+            val cartQuantity: Int = model.cart_quantity.toInt()
+            if (cartQuantity < model.stock_quantity.toInt()){
+                val itemHashMap = HashMap<String,Any>()
+                itemHashMap[Constants.CART_QUANTITY] = (cartQuantity + 1).toString()
 
+                if (context is CartListActivity){
+                    context.showProgressDialog()
+                }
+                FirestoreClass().updateMyCart(context, model.id, itemHashMap)
+            }else{
+                if (context is CartListActivity){
+                    context.showErrorSnackBar(
+                        context.resources.getString(
+                            R.string.msg_for_available_stock,
+                            model.stock_quantity
+                    ),
+                    true)
+                }
+            }
+        }
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/hr/util/Constants.kt
+++ app/src/main/java/hr/util/Constants.kt
@@ -36,6 +36,8 @@ object Constants {
     const val CART_ITEMS: String = "cart_items"
     const val PRODUCT_ID: String = "product_id"
 
+    const val CART_QUANTITY: String = "cart_quantity"
+
     fun showImageChooser(activity: Activity){
         //An intent for launching the image selection of phone storage
         val galleryIntent = Intent(
