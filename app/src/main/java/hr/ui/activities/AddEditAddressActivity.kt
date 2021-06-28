44543a9af0a836be9b5048fe75fd1796e41d3ae1
Dominik277 <dominikdoric277@gmail.com>
2021-06-28 21:17:53 +0200

    Dodana nova aktivnost AddEditAddressActivity.kt.

---
+++ app/src/main/java/hr/ui/activities/AddEditAddressActivity.kt
@@ -0,0 +1,12 @@
+package hr.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hr.dominik.ribolovnodrustvojaksic.R
+
+class AddEditAddressActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit_address)
+    }
+}
