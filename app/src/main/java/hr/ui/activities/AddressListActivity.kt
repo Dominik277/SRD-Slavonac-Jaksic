ee0e0d62c533c784caeb347b72a140c316694e76
Dominik277 <dominikdoric277@gmail.com>
2021-06-28 21:12:45 +0200

    Dodan action bar u AddressListActivity.kt.

--- app/src/main/java/hr/ui/activities/AddressListActivity.kt
+++ app/src/main/java/hr/ui/activities/AddressListActivity.kt
@@ -3,10 +3,30 @@ package hr.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import hr.dominik.ribolovnodrustvojaksic.R
+import hr.dominik.ribolovnodrustvojaksic.databinding.ActivityAddressListBinding
 
 class AddressListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddressListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_address_list)
+        binding = ActivityAddressListBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        setupActionBar()
     }
+
+    private fun setupActionBar(){
+        setSupportActionBar(binding.toolbarAddressListActivity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_black_24)
+        }
+
+        binding.toolbarAddressListActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+
 }
