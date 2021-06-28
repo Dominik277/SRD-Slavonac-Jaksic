12eb155eaeab197259fb0cdb180b12df6f89d5bd
Dominik277 <dominikdoric277@gmail.com>
2021-06-28 22:05:35 +0200

    Napravljena metoda koja sluzi za provjeravanje podataka koji su uneseni unutar Edit text-a.

--- app/src/main/java/hr/ui/activities/AddEditAddressActivity.kt
+++ app/src/main/java/hr/ui/activities/AddEditAddressActivity.kt
@@ -2,11 +2,12 @@ package hr.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import hr.dominik.ribolovnodrustvojaksic.R
 import hr.dominik.ribolovnodrustvojaksic.databinding.ActivityAddEditAddressBinding
 import hr.dominik.ribolovnodrustvojaksic.databinding.ActivityAddressListBinding
 
-class AddEditAddressActivity : AppCompatActivity() {
+class AddEditAddressActivity : BaseActivity() {
 
     private lateinit var binding: ActivityAddEditAddressBinding
 
@@ -18,16 +19,60 @@ class AddEditAddressActivity : AppCompatActivity() {
         setupActionBar()
     }
 
-    private fun setupActionBar(){
+    private fun setupActionBar() {
         setSupportActionBar(binding.toolbarAddEditAddressActivity)
 
         val actionBar = supportActionBar
-        if (actionBar != null){
+        if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true)
             actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_black_24)
         }
 
         binding.toolbarAddEditAddressActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    private fun validateData(): Boolean {
+        return when {
+            TextUtils.isEmpty(binding.etFullName.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_please_enter_full_name),
+                    true
+                )
+                false
+            }
+            TextUtils.isEmpty(binding.etPhoneNumber.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_please_enter_phone_number),
+                    true
+                )
+                false
+            }
+            TextUtils.isEmpty(binding.etAddress.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_please_enter_address),
+                    true
+                )
+                false
+            }
+            TextUtils.isEmpty(binding.etZipCode.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_please_enter_zip_code),
+                    true
+                )
+                false
+            }
+            binding.rbOther.isChecked && TextUtils.isEmpty(
+                binding.etZipCode.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_please_enter_zip_code),
+                    true
+                )
+                false
+            }
+            else -> {
+                true
+            }
+        }
     }
 
 }
