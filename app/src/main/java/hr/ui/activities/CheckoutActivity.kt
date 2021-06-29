573d03d6b9afd9d05902a192c71de98bc535fb9e
Dominik277 <dominikdoric277@gmail.com>
2021-06-29 11:37:38 +0200

    Kreiran vizualni dio za activity_checkout.xml.

---
+++ app/src/main/java/hr/ui/activities/CheckoutActivity.kt
@@ -0,0 +1,18 @@
+package hr.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hr.dominik.ribolovnodrustvojaksic.R
+import hr.dominik.ribolovnodrustvojaksic.databinding.ActivityCheckoutBinding
+
+class CheckoutActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCheckoutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCheckoutBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
