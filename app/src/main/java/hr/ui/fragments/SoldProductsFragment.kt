a8269005ca97a9b8261280a37b958880126f8c87
Dominik277 <dominikdoric277@gmail.com>
2021-06-30 01:37:03 +0200

    Dodano par sitnica i vizualni dio za fragment_sold_products.xml.

--- app/src/main/java/hr/ui/fragments/SoldProductsFragment.kt
+++ app/src/main/java/hr/ui/fragments/SoldProductsFragment.kt
@@ -1,7 +1,26 @@
 package hr.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import hr.dominik.ribolovnodrustvojaksic.databinding.FragmentSoldProductsBinding
+
 class SoldProductsFragment: BaseFragment() {
 
+    private lateinit var binding: FragmentSoldProductsBinding
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentSoldProductsBinding.bind(view)
+    }
 
 }
