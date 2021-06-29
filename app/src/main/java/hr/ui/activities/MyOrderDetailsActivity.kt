a7b7ea306358ca091852e2dac072b446d400e047
Dominik277 <dominikdoric277@gmail.com>
2021-06-29 20:25:40 +0200

    Kreiran vizualni dio za activity_my_order_details.xml.

---
+++ app/src/main/java/hr/ui/activities/MyOrderDetailsActivity.kt
@@ -0,0 +1,17 @@
+package hr.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hr.dominik.ribolovnodrustvojaksic.R
+import hr.dominik.ribolovnodrustvojaksic.databinding.ActivityMyOrderDetailsBinding
+
+class MyOrderDetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMyOrderDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMyOrderDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
