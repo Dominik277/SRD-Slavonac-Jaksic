bdb026afbe182aa932cab02d271a57476127714a
Dominik277 <dominikdoric277@gmail.com>
2021-06-30 02:43:11 +0200

    Implementirano to da se prikazuju podacti koji su poslati s intentom.

--- app/src/main/java/hr/ui/activities/SoldProductDetailsActivity.kt
+++ app/src/main/java/hr/ui/activities/SoldProductDetailsActivity.kt
@@ -2,18 +2,87 @@ package hr.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import hr.dominik.ribolovnodrustvojaksic.R
+import hr.dominik.ribolovnodrustvojaksic.databinding.ActivitySoldProductDetailsBinding
 import hr.model.SoldProduct
 import hr.util.Constants
+import hr.util.GlideLoader
+import java.text.SimpleDateFormat
+import java.util.*
 
 class SoldProductDetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySoldProductDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sold_product_details)
+        binding = ActivitySoldProductDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupActionBar()
 
         var productDetails: SoldProduct = SoldProduct()
         if (intent.hasExtra(Constants.EXTRA_SOLD_PRODUCT_DETAILS)){
             productDetails = intent.getParcelableExtra(Constants.EXTRA_SOLD_PRODUCT_DETAILS)!!
         }
+
+        setupUI(productDetails)
     }
+
+    private fun setupActionBar() {
+
+        setSupportActionBar(binding.toolbarSoldProductDetailsActivity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24)
+        }
+
+        binding.toolbarSoldProductDetailsActivity.setNavigationOnClickListener { onBackPressed() }
+
+    }
+
+    private fun setupUI(productDetails: SoldProduct) {
+
+        binding.tvSoldProductDetailsId.text = productDetails.order_id
+
+        // Date Format in which the date will be displayed in the UI.
+        val dateFormat = "dd MMM yyyy HH:mm"
+        // Create a DateFormatter object for displaying date in specified format.
+        val formatter = SimpleDateFormat(dateFormat, Locale.getDefault())
+
+        // Create a calendar object that will convert the date and time value in milliseconds to date.
+        val calendar: Calendar = Calendar.getInstance()
+        calendar.timeInMillis = productDetails.order_date
+        binding.tvSoldProductDetailsDate.text = formatter.format(calendar.time)
+
+        GlideLoader(this@SoldProductDetailsActivity).loadProductPicture(
+            productDetails.image,
+            binding.ivProductItemImage
+        )
+
+        binding.tvProductItemName.text = productDetails.title
+        binding.tvProductItemPrice.text ="$${productDetails.price}"
+        binding.tvSoldProductTotalAmount.text = productDetails.sold_quantity
+
+        binding.tvSoldDetailsAddressType.text = productDetails.address.type
+        binding.tvSoldDetailsFullName.text = productDetails.address.name
+        binding.tvSoldDetailsAddress.text =
+            "${productDetails.address.address}, ${productDetails.address.zipCode}"
+        binding.tvSoldDetailsAdditionalNote.text = productDetails.address.additionalNote
+
+        if (productDetails.address.otherDetails.isNotEmpty()) {
+            binding.tvSoldDetailsOtherDetails.visibility = View.VISIBLE
+            binding.tvSoldDetailsOtherDetails.text = productDetails.address.otherDetails
+        } else {
+            binding.tvSoldDetailsOtherDetails.visibility = View.GONE
+        }
+        binding.tvSoldDetailsMobileNumber.text = productDetails.address.mobileNumber
+
+        binding.tvSoldProductSubTotal.text = productDetails.sub_total_amount
+        binding.tvSoldProductShippingCharge.text = productDetails.shipping_charge
+        binding.tvSoldProductTotalAmount.text = productDetails.total_amount
+    }
+
 }
